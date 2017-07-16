adf646a1f29e474ba98b9ab6accb592632d03699
ppalyga <homme.ex.machina@gmail.com>
2017-07-16 18:21:24 +0200

    ADD about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,32 @@
     var nav = document.querySelector('.navigation'),
         hamburger = document.querySelector('.navigation__hamburger');
         currentSection = document.querySelector('.navigation__current-section'),
-        navigationList = document.querySelector('.navigation__list');
+        navigationList = document.querySelector('.navigation__list'),
+        barTop = document.querySelector('.navigation__bar--top'),
+        barMiddle = document.querySelector('.navigation__bar--middle'),
+        barBottom = document.querySelector('.navigation__bar--bottom');
 
     function initializeMenu() {
         nav.style.display = 'flex';
     }
 
     function toggleMenu() {
-        hamburger.addEventListener('click', function () {
+        function menuEvents() {
             nav.classList.toggle('navigation--full-screen');
             currentSection.classList.toggle('navigation__current-section--hidden');
             navigationList.classList.toggle('navigation__list--active');
+            barMiddle.classList.toggle('navigation__bar--hidden');
+            barBottom.classList.toggle('navigation__bar--longer');
+            barBottom.classList.toggle('navigation__bar--rotate-left');
+            barTop.classList.toggle('navigation__bar--rotate-right');
+        }
+
+        hamburger.addEventListener('click', function () {
+            menuEvents();
         });
 
         navigationList.addEventListener('click', function () {
-            nav.classList.remove('navigation--full-screen');
-            navigationList.classList.remove('navigation__list--active');
+            menuEvents();
         });
     }
 
