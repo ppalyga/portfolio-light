9d76ecffbcd210b92270e12e8df476ca5815f637
ppalyga <homme.ex.machina@gmail.com>
2017-07-18 22:17:16 +0200

    TEMP b

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+(function () {
+    var nav = document.querySelector('.navigation'),
+        hamburger = document.querySelector('.navigation__hamburger'),
+        currentSection = document.querySelector('.navigation__current-section'),
+        navigationList = document.querySelector('.navigation__list'),
+        barTop = document.querySelector('.navigation__bar--top'),
+        barMiddle = document.querySelector('.navigation__bar--middle'),
+        barBottom = document.querySelector('.navigation__bar--bottom');
+
+    function initializeMenu() {
+        nav.style.display = 'flex';
+    }
+
+    function toggleMenu() {
+        function menuEvents() {
+            nav.classList.toggle('navigation--full-screen');
+            currentSection.classList.toggle('navigation__current-section--hidden');
+            navigationList.classList.toggle('navigation__list--active');
+            barMiddle.classList.toggle('navigation__bar--hidden');
+            barBottom.classList.toggle('navigation__bar--longer');
+            barBottom.classList.toggle('navigation__bar--rotate-left');
+            barTop.classList.toggle('navigation__bar--rotate-right');
+        }
+
+        hamburger.addEventListener('click', function () {
+            menuEvents();
+        });
+
+        navigationList.addEventListener('click', function () {
+            menuEvents();
+        });
+    }
+
+    function initializeSkillsSlider() {
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            loop: true
+        });
+    }
+
+    initializeMenu();
+    toggleMenu();
+    initializeSkillsSlider();
+
+})();
