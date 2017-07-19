1459735b1e27c58910dce87cfed8c57686a703af
ppalyga <homme.ex.machina@gmail.com>
2017-07-19 20:03:31 +0200

    ADD tablet version

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,19 @@
         barMiddle = document.querySelector('.navigation__bar--middle'),
         barBottom = document.querySelector('.navigation__bar--bottom');
 
+    function throttle(callback, limit) {
+        var wait = false;
+        return function () {
+            if (!wait) {
+                callback.call();
+                wait = true;
+                setTimeout(function () {
+                    wait = false;
+                }, limit);
+            }
+        };
+    }
+
     function initializeMenu() {
         nav.style.display = 'flex';
     }
@@ -39,8 +52,40 @@
         });
     }
 
+    function changeActiveSection() {
+        var activeSection = document.querySelector(".navigation__current-section"),
+            screenWidth = window.innerWidth;
+
+        window.addEventListener("resize", throttle(function () {
+            screenWidth = window.innerWidth;
+        }, 500));
+
+        window.addEventListener("scroll", throttle(function () {
+            if (screenWidth < 600) {
+                if (window.pageYOffset < 900) {
+                    activeSection.textContent = "ABOUT ME";
+                } else if (window.pageYOffset >= 900 & window.pageYOffset < 1400) {
+                    activeSection.textContent = "SKILLS";
+                } else if (window.pageYOffset >= 1400 & window.pageYOffset < 1850) {
+                    activeSection.textContent = "PROJECTS";
+                } else {
+                    activeSection.textContent = "CONTACT";
+                }
+            } else {
+                if (window.pageYOffset < 400) {
+                    activeSection.textContent = "ABOUT ME";
+                } else if (window.pageYOffset >= 400 & window.pageYOffset < 920) {
+                    activeSection.textContent = "SKILLS";
+                } else {
+                    activeSection.textContent = "PROJECTS & CONTACT";
+                }
+            }
+        }, 100));
+    }
+
     initializeMenu();
     toggleMenu();
     initializeSkillsSlider();
+    changeActiveSection();
 
 })();
